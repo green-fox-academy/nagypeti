de404444b9dc28f2f944fb15c94f0870278ffa02
nagypeti <nagypeti222@gmail.com>
2018-10-29 15:34:31 +0100

    Add: Finishing Variables and UserInput workshop tasks

---
+++ week_02/day_01/src/com/company/Variables/Cuboid.java
@@ -0,0 +1,15 @@
+package com.company.Variables;
+
+public class Cuboid {
+
+    public static void main(String[] args) {
+
+        // Write a program that stores 3 sides of a cuboid as variables (doubles)
+        // The program should write the surface area and volume of the cuboid like:
+        //
+        // Surface Area: 600
+        // Volume: 1000
+
+
+    }
+}
---
+++ week_02/day_01/src/com/company/Variables/bmi.java
@@ -0,0 +1,17 @@
+package com.company.Variables;
+
+public class bmi {
+
+    public static void main(String[] args) {
+
+        double massInKg = 81.2;
+        double heightInM = 1.78;
+
+        double bmi = massInKg / Math.pow(heightInM,2);
+
+        System.out.println("Your BMI point is: " + bmi);
+
+        // Print the Body mass index (BMI) based on these values
+
+    }
+}
