d7a614f95e4747d47a78b97b595a460f5a7736ae
nagypeti <nagypeti222@gmail.com>
2018-11-07 12:29:35 +0100

    Add: fourRectangle and fourRectangleHourGlass

---
+++ week_02/day_03/src/com/week02/day03/workshop/fourRectangles.java
@@ -0,0 +1,54 @@
+package com.week02.day03.workshop;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class fourRectangles {
+
+    public static void mainDraw(Graphics graphics){
+        // draw four different size and color rectangles.
+        // avoid code duplication.
+
+        for (int i = 0; i < 4; i++) {
+            drawRectangle(graphics);
+        }
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void drawRectangle (Graphics graphics){
+
+        int xStartPos = 1 + (int)(Math.random() * 100);
+        int yStartPos = 1 + (int)(Math.random() * 100);
+        int width = 1 + (int)(Math.random() * 100);
+        int length = 1 + (int)(Math.random() * 100);
+
+        graphics.setColor(new Color(1 + (int)(Math.random() * 256),1 + (int)(Math.random() * 256), 1 + (int)(Math.random() * 256)));
+        graphics.fillRect(xStartPos,yStartPos,width,length);
+
+    }
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
