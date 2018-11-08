bc66e8192abfbb735f2a5c6bd2e953f762e89632
nagypeti <nagypeti222@gmail.com>
2018-11-08 14:18:16 +0100

    Update: drawSquare an drawDiagonal

--- week_01/day_03/src/com/week02/day01/Loops/drawDiagonal.java
+++ week_01/day_03/src/com/week02/day01/Loops/drawDiagonal.java
@@ -1,5 +1,7 @@
 package com.week02.day01.Loops;
 
+import java.util.Scanner;
+
 public class drawDiagonal {
 
     public static void main(String[] args) {
@@ -16,5 +18,25 @@ public class drawDiagonal {
         //
         // The square should have as many lines as the number was
 
+        System.out.println("Give a number: ");
+        Scanner input = new Scanner(System.in);
+        int favNum = input.nextInt();
+        drawSquare(favNum);
+
     }
+
+    public static void drawSquare (int height) {
+
+        for (int i = 0; i < height; i++) {
+                for (int j = 0; j < height; j++) {
+                    if (i == 0 || i == height - 1 || i == j || j == 0 || j == height -1){
+                        System.out.print("%");
+                    } else {
+                        System.out.print(" ");
+                    }
+            }
+            System.out.println();
+        }
+    }
+
 }
--- week_01/day_03/src/com/week02/day01/Loops/drawSquare.java
+++ week_01/day_03/src/com/week02/day01/Loops/drawSquare.java
@@ -6,39 +6,22 @@ public class drawSquare {
 
     public static void main(String[] args) {
 
-        // Write a program that reads a number from the standard input, then draws a
-        // square like this:
-        //
-        //
-        // %%%%%%
-        // %    %
-        // %    %
-        // %    %
-        // %    %
-        // %%%%%%
-        //
-        // The square should have as many lines as the number was
-
         System.out.println("Give a number: ");
-
         Scanner input = new Scanner(System.in);
-
         int favNum = input.nextInt();
-
         drawSquare(favNum);
 
     }
 
-    public static void drawSquare (int heightOfSquare) {
+    public static void drawSquare (int height) {
 
-        String whiteSpace = " ";
-        for (int i = 0; i < heightOfSquare; i++) {
-            if (i == 0 || i == heightOfSquare - 1) {
-                for (int j = 0; j < heightOfSquare; j++) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < height; j++) {
+                if (i == 0 || i == height - 1 || j == 0 || j == height - 1) {
                     System.out.print("%");
+                } else {
+                    System.out.print(" ");
                 }
-            } else {
-                System.out.print("%" + whiteSpace.repeat(heightOfSquare - 2) + "%");
             }
             System.out.println();
         }
