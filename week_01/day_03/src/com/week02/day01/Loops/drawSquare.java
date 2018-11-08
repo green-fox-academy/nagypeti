7fc10fadfcbbdaa406d05a4c11462cd99bd7495a
nagypeti <nagypeti222@gmail.com>
2018-11-08 12:46:51 +0100

    Add: drawSquare

--- week_01/day_03/src/com/week02/day01/Loops/drawSquare.java
+++ week_01/day_03/src/com/week02/day01/Loops/drawSquare.java
@@ -1,9 +1,46 @@
 package com.week02.day01.Loops;
 
+import java.util.Scanner;
+
 public class drawSquare {
 
     public static void main(String[] args) {
 
+        // Write a program that reads a number from the standard input, then draws a
+        // square like this:
+        //
+        //
+        // %%%%%%
+        // %    %
+        // %    %
+        // %    %
+        // %    %
+        // %%%%%%
+        //
+        // The square should have as many lines as the number was
 
+        System.out.println("Give a number: ");
+
+        Scanner input = new Scanner(System.in);
+
+        int favNum = input.nextInt();
+
+        drawSquare(favNum);
+
+    }
+
+    public static void drawSquare (int heightOfSquare) {
+
+        String whiteSpace = " ";
+        for (int i = 0; i < heightOfSquare; i++) {
+            if (i == 0 || i == heightOfSquare - 1) {
+                for (int j = 0; j < heightOfSquare; j++) {
+                    System.out.print("%");
+                }
+            } else {
+                System.out.print("%" + whiteSpace.repeat(heightOfSquare - 2) + "%");
+            }
+            System.out.println();
+        }
     }
 }
