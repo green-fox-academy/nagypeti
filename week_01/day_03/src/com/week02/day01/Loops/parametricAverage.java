e71151302baf573d65a1b8bc396c267d124fe727
nagypeti <nagypeti222@gmail.com>
2018-11-08 16:31:45 +0100

    Add: parametricAverage

--- week_01/day_03/src/com/week02/day01/Loops/parametricAverage.java
+++ week_01/day_03/src/com/week02/day01/Loops/parametricAverage.java
@@ -1,18 +1,34 @@
 package com.week02.day01.Loops;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class parametricAverage {
 
     public static void main(String[] args) {
 
-        // Write a program that asks for a number.
-        // It would ask this many times to enter an integer,
-        // if all the integers are entered, it should print the sum and average of these
-        // integers like:
-        //
-        // Sum: 22, Average: 4.4
+        System.out.println(parametricAvrg(getNumbers()));
 
+    }
 
-
-
+    public static int [] getNumbers () {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Give me a number: ");
+        int nrToAsk = scanner.nextInt();
+        int [] numbersToWorkWith = new int [nrToAsk];
+        System.out.println("Give me " + nrToAsk + " number:");
+        for (int i = 0; i < nrToAsk; i++) {
+            numbersToWorkWith[i] = scanner.nextInt();
+        }
+        return numbersToWorkWith;
+    }
+    public static String parametricAvrg (int [] inputArray) {
+        int sum = 0;
+        for (int j = 0; j < inputArray.length; j++) {
+            sum += inputArray[j];
+        }
+        double parAverage = (double) sum / inputArray.length;
+        String returnValue = "Sum: " + sum + ", Average: " + parAverage;
+        return returnValue;
     }
 }
