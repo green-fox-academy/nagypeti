d3cfc142bb936c9f50d595244242b9d16b39a1b0
nagypeti <nagypeti222@gmail.com>
2018-11-13 12:43:37 +0100

    Add: animal

---
+++ week_03/day_02/src/animal/Animal.java
@@ -0,0 +1,25 @@
+package animal;
+
+public class Animal {
+
+  int hunger;
+  int thirst;
+
+  public Animal () {
+    this(50,50);
+  }
+  public Animal (int hunger, int thirst) {
+    this.hunger = hunger;
+    this.thirst = thirst;
+  }
+  public void eat () {
+    this.hunger--;
+  }
+  public void drink () {
+    this.thirst--;
+  }
+  public void play () {
+    this.thirst++;
+    this.hunger++;
+  }
+}
