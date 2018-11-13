9e9c2b7c6172d78cf11c1b55a5f1de172608fba4
nagypeti <nagypeti222@gmail.com>
2018-11-13 12:59:06 +0100

    Add: Sharpie

---
+++ week_03/day_02/src/sharpie/Main.java
@@ -0,0 +1,16 @@
+package sharpie;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Sharpie boat = new Sharpie("blue", 2.0f);
+
+    boat.use();
+    boat.use();
+    boat.use();
+
+    System.out.println(boat.inkAmount);
+
+  }
+}
