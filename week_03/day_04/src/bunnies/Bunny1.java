50433570e769e372bcf90b5a58f53539ea266667
nagypeti <nagypeti222@gmail.com>
2018-11-15 12:13:20 +0100

    Add: Bunny1

---
+++ week_03/day_04/src/bunnies/Bunny1.java
@@ -0,0 +1,22 @@
+package bunnies;
+
+// We have a number of bunnies and each bunny has two big floppy ears.
+// We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+
+public class Bunny1 {
+
+  public static void main(String[] args) {
+
+    System.out.println(earCounter(12,2));
+
+  }
+
+  public static int earCounter(int nrOfAnimals, int nrOfEarPerAnimal) {
+
+    if (nrOfAnimals < 1)
+      return 0;
+
+    return earCounter(nrOfAnimals -1, nrOfEarPerAnimal) + nrOfEarPerAnimal;
+  }
+
+}
