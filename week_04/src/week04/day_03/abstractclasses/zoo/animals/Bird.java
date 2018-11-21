c8d827b7dce7fe1a72b2a08143e6e9e93788c1e8
nagypeti <nagypeti222@gmail.com>
2018-11-21 15:53:50 +0100

    Upd: implement Flyable interface to Bird class

--- week_04/src/week04/day_03/abstractclasses/zoo/animals/Bird.java
+++ week_04/src/week04/day_03/abstractclasses/zoo/animals/Bird.java
@@ -1,6 +1,8 @@
 package week04.day_03.abstractclasses.zoo.animals;
 
-public class Bird extends EggLayers {
+import week04.day_03.interfaces.flyable.flyable.Flyable;
+
+public class Bird extends EggLayers implements Flyable {
 
   public Bird() {
     this("");
@@ -8,6 +10,21 @@ public class Bird extends EggLayers {
 
   public Bird(String name) {
     this.name = name;
+  }
+
+  @Override
+  public void land() {
+
+  }
+
+  @Override
+  public void fly() {
+
+  }
+
+  @Override
+  public void takeOff() {
+
   }
 
 }
