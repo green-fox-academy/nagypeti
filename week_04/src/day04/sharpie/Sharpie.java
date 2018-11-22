bfd377d57b42c1d623275d93d5a2ca4f9c6cd804
nagypeti <nagypeti222@gmail.com>
2018-11-22 14:55:39 +0100

    Add: W04D04 sharpie task src package

---
+++ week_04/src/day04/sharpie/Sharpie.java
@@ -0,0 +1,19 @@
+package day04.sharpie;
+
+public class Sharpie {
+
+  String color;
+  float width;
+  public float inkAmount;
+
+  public Sharpie(String color, float width) {
+    this.color = color;
+    this.width = width;
+    this.inkAmount = 100;
+  }
+
+  public void use() {
+    this.inkAmount--;
+  }
+
+}
