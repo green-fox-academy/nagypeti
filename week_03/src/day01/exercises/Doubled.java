ffe253cf985ffd60a47e1035435bce1277673e38
nagypeti <nagypeti222@gmail.com>
2018-12-05 11:58:04 +0100

    Upd: created new structure for Double task

---
+++ week_03/src/day01/exercises/Doubled.java
@@ -0,0 +1,14 @@
+package day01.exercises;
+
+public class Doubled {
+
+  public static void main(String[] args) {
+    // Create a method that decrypts the duplicated-chars.txt
+
+    String asd = "0,1,2,4,";
+
+    Integer.parseInt(asd.split(","));
+
+  }
+}
+
