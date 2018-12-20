91f473226728c759e096c471c89b9ee4d67b8322
nagypeti <nagypeti222@gmail.com>
2018-12-20 12:32:16 +0100

    Modify: new flder structure for w08 d01 and d03 projects

---
+++ week_08/01_todowitddatabase/h2todo/src/test/java/com/w08d01/dbintegration/DbintegrationApplicationTests.java
@@ -0,0 +1,17 @@
+package com.w08d01.dbintegration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DbintegrationApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
+
