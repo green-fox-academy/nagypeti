e892450bd87bd76ead824b5ccacd297b57024c1d
nagypeti <nagypeti222@gmail.com>
2019-01-08 15:12:48 +0100

    Add: w09 d02 first task finished with tests

---
+++ week_09/day02_rest_test/src/main/java/com/guardians/ofthegalaxy/controller/GuardianController.java
@@ -0,0 +1,22 @@
+package com.guardians.ofthegalaxy.controller;
+
+import com.guardians.ofthegalaxy.model.Groot;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import com.guardians.ofthegalaxy.Error;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/groot")
+public class GuardianController {
+
+  @GetMapping
+  public ResponseEntity<Object> grootTranslator(@RequestParam(required = false) String message) {
+    if (message == null || message.equals("")) {
+      return new ResponseEntity(new Error("I am Groot!"), HttpStatus.BAD_REQUEST);
+    }
+    return new ResponseEntity(new Groot(message), HttpStatus.OK);
+  }
+
+}
---
+++ week_09/day02_rest_test/src/test/java/com/guardians/ofthegalaxy/controller/GuardianControllerTest.java
@@ -0,0 +1,38 @@
+package com.guardians.ofthegalaxy.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(GuardianController.class)
+public class GuardianControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  public void grootTranslator_testStatusOk() throws Exception{
+    this.mockMvc.perform(get("/groot").param("message", "asd"))
+            .andExpect(jsonPath("$.received",is("asd")))
+            .andExpect(jsonPath("$.translated",is("I am Groot!")))
+            .andExpect(status().isOk());
+  }
+
+  @Test
+  public void grootTranslator_testBadRequest() throws Exception{
+    this.mockMvc.perform(get("/groot").param("message", ""))
+            .andExpect(jsonPath("$.error",is("I am Groot!")))
+            .andExpect(status().isBadRequest());
+  }
+
+}
