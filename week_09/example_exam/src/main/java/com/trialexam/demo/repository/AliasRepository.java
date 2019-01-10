539953deee0a39eb0921fd57c8c3b291c8c96f46
nagypeti <nagypeti222@gmail.com>
2019-01-10 19:31:43 +0100

    Upd: rewrited  succes message template in index.html

--- week_09/example_exam/src/main/java/com/trialexam/demo/repository/AliasRepository.java
+++ week_09/example_exam/src/main/java/com/trialexam/demo/repository/AliasRepository.java
@@ -1,7 +1,6 @@
 package com.trialexam.demo.repository;
 
 import com.trialexam.demo.model.Alias;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
