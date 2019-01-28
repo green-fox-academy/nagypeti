50bade66665a9b5a6ed1998615db38f653d6b00b
nagypeti <nagypeti222@gmail.com>
2019-01-28 16:36:17 +0100

    Upd: rebuilt Dockerfile for aws deployment with jenkins

--- project_phase/week_00/aws_test/src/main/java/com/test/demo/DemoApplication.java
+++ project_phase/week_00/aws_test/src/main/java/com/test/demo/DemoApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
 
-	@GetMapping
+	@GetMapping("/")
 	public String hello() {
 		return "Hello";
 	}
