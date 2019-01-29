629263ea31279f00f599a3c0c1a6247b760b0ad0
nagypeti <nagypeti222@gmail.com>
2019-01-29 16:24:33 +0100

    working terraform config with sonarqube with t3.medium ec2 instance using the default securitygroup

---
+++ project_phase/week_02/terraform_basic/main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  profile                 = "${var.profile}"
+  shared_credentials_file = "~/.aws/credentials"
+  region                  = "${var.aws_region}"
+}
+
+resource "aws_instance" "web1" {
+  ami           = "ami-6d27a913"
+  instance_type = "t3.medium"
+  key_name      = "${var.key_name}"
+
+  connection {
+    user        = "ec2-user"
+    type        = "ssh"
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  tags {
+    Name = "isitworking?"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install -y docker",
+      "sudo systemctl enable docker.service",
+      "sudo systemctl start docker.service",
+      "sudo docker run -p 9000:9000 sonarqube",
+    ]
+  }
+}
