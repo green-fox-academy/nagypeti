be44c000d6fbe0933120df0a00582c1bc569283f
nagypeti <nagypeti222@gmail.com>
2019-02-08 12:25:32 +0100

    Upd: S3 bucket is working as a static website, with terraform configuration

--- project_phase/week_03/terraform_Frontend_deployment_with_AWS_S3_bucket/main.tf
+++ project_phase/week_03/terraform_Frontend_deployment_with_AWS_S3_bucket/main.tf
@@ -1,22 +1,40 @@
-resource "aws_s3_bucket" "deploy_frontend_website_bucket" {
+# resource "aws_s3_bucket" "logs" {
+#   bucket = "${var.website_bucket_name}-site-logs"
+#   acl    = "log-delivery-write-read"
+# }
+
+data "template_file" "s3_public_policy" {
+  template = "${file("${var.workdir_path}/policy.json")}"
+
+  vars {
+    bucket_name = "${var.website_bucket_name}"
+  }
+}
+
+resource "aws_s3_bucket" "website_bucket" {
   bucket = "${var.website_bucket_name}"
   acl    = "public-read"
-  policy = "${file("${var.workdir_path}/policy.json")}"
+  policy = "${data.template_file.s3_public_policy.rendered}"
 
   tags {
-    Name        = "Website"
+    name        = "${var.website_bucket_name}"
     Environment = "Dev"
   }
 
-  cors_rule {
-    allowed_headers = ["*"]
-    allowed_methods = ["PUT", "POST"]
-    allowed_origins = ["*"]
-    expose_headers  = ["ETag"]
-    max_age_seconds = 3000
-  }
+  # logging {
+  #   target_bucket = "${var.website_bucket_name}"
+  #   target_prefix = "log-${var.website_bucket_name}"
+  # }
 
   website {
     index_document = "index.html"
   }
+}
+
+resource "aws_s3_bucket_object" "index" {
+  bucket       = "${aws_s3_bucket.website_bucket.bucket}"
+  key          = "index.html"
+  source       = "${var.workdir_path}/index.html"
+  content_type = "text/html"
+  etag         = "${md5(file("${var.workdir_path}/index.html"))}"
 }
