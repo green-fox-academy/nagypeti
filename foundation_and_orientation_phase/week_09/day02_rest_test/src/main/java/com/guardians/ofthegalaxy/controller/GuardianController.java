d015a4797a4dc083acab8502d497d24bc409709d
nagypeti <nagypeti222@gmail.com>
2019-02-12 09:36:57 +0100

    Upd: test are updated

--- foundation_and_orientation_phase/week_09/day02_rest_test/src/main/java/com/guardians/ofthegalaxy/controller/GuardianController.java
+++ foundation_and_orientation_phase/week_09/day02_rest_test/src/main/java/com/guardians/ofthegalaxy/controller/GuardianController.java
@@ -1,9 +1,9 @@
 package com.guardians.ofthegalaxy.controller;
 
+import com.guardians.ofthegalaxy.Error;
 import com.guardians.ofthegalaxy.model.Groot;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import com.guardians.ofthegalaxy.Error;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
